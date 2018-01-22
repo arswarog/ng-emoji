2bdea2128ab7bccf219ba163571ca80875775dad
Alexey Savchuk <sabo@devpreview.ru>
2018-01-22 09:53:11 +0300

    Demo: bugfix emoji bundles path

--- webpack.demo.js
+++ webpack.demo.js
@@ -16,290 +16,299 @@ const extractLess = new ExtractTextPlugin({
     filename: "[name].[contenthash].css"
 });
 
+const isDevServer = process.argv[1].indexOf('webpack-dev-server') !== -1;
+
 /**
  * Webpack configuration
  *
  * See: http://webpack.github.io/docs/configuration.html#cli
  */
-module.exports = {
-    /**
-     * Developer tool to enhance debugging
-     *
-     * See: http://webpack.github.io/docs/configuration.html#devtool
-     * See: https://github.com/webpack/docs/wiki/build-performance#sourcemaps
-     */
-    devtool: 'source-map',
-
-    /*
-     * The entry point for the bundle
-     *
-     * See: http://webpack.github.io/docs/configuration.html#entry
-     */
-    entry: {
-        'demo': path.resolve(__dirname, './src/demo/main.browser.ts')
-    },
-
-    /**
-     * Options affecting the output of the compilation.
-     *
-     * See: http://webpack.github.io/docs/configuration.html#output
-     */
-    output: {
+module.exports = function () {
+    let config = {
         /**
-         * The output directory as absolute path (required).
+         * Developer tool to enhance debugging
          *
-         * See: http://webpack.github.io/docs/configuration.html#output-path
+         * See: http://webpack.github.io/docs/configuration.html#devtool
+         * See: https://github.com/webpack/docs/wiki/build-performance#sourcemaps
          */
-        path: path.resolve(__dirname, './dist-demo'),
+        devtool: 'source-map',
 
-        /**
-         * Specifies the name of each output file on disk.
-         * IMPORTANT: You must not specify an absolute path here!
-         *
-         * See: http://webpack.github.io/docs/configuration.html#output-filename
-         */
-        filename: '[name].[chunkhash].js',
-
-        /**
-         * Chunk filename with hash.
-         *
-         * See: https://github.com/webpack/webpack/tree/master/examples/chunkhash#webpackconfigjs
-         */
-        chunkFilename: "[chunkhash].js",
-
-        /**
-         * The filename of the SourceMaps for the JavaScript files.
-         * They are inside the output.path directory.
-         *
-         * See: http://webpack.github.io/docs/configuration.html#output-sourcemapfilename
-         */
-        sourceMapFilename: '[file].map',
-
-        /**
-         * Export the bundle as library
-         * See: https://webpack.js.org/configuration/output/#output-librarytarget
-         */
-        libraryTarget: 'var'
-    },
-
-    /**
-     * Options affecting the resolving of modules.
-     *
-     * See: http://webpack.github.io/docs/configuration.html#resolve
-     */
-    resolve: {
         /*
-         * An array of extensions that should be used to resolve modules.
+         * The entry point for the bundle
          *
-         * See: http://webpack.github.io/docs/configuration.html#resolve-extensions
+         * See: http://webpack.github.io/docs/configuration.html#entry
          */
-        extensions: ['.ts', '.js', '.json'],
-
-        // An array of directory names to be resolved to the current directory
-        modules: [
-            path.resolve(__dirname, './src'),
-            path.resolve(__dirname, './node_modules')
-        ]
-    },
-
-    /*
-     * Options affecting the normal modules.
-     *
-     * See: http://webpack.github.io/docs/configuration.html#module
-     */
-    module: {
-        /**
-         * An array of applied pre and post loaders.
-         *
-         * See: https://webpack.js.org/configuration/module/#rule-enforce
-         */
-        rules: [
-            /**
-             * Static analysis linter for TypeScript advanced options configuration
-             * Description: An extensible linter for the TypeScript language.
-             *
-             * See: https://github.com/wbuchwalter/tslint-loader
-             */
-            {
-                test: /\.ts$/,
-                enforce: 'pre',
-                loader: 'tslint-loader',
-                options: {
-                    emitErrors: true,
-                    failOnHint: true,
-                    resourcePath: 'src'
-                }
-            },
-
-            /**
-             * Typescript loader support for .ts
-             *
-             * Component Template/Style integration using `angular2-template-loader`
-             * Angular 2 lazy loading (async routes) via `ng-router-loader`
-             *
-             * `ng-router-loader` expects vanilla JavaScript code, not TypeScript code. This is why the
-             * order of the loader matter.
-             *
-             * See: https://github.com/s-panferov/awesome-typescript-loader
-             * See: https://github.com/TheLarkInn/angular2-template-loader
-             * See: https://github.com/shlomiassaf/ng-router-loader
-             */
-            {
-                test: /\.ts$/,
-                use: [
-                    {
-                        loader: 'awesome-typescript-loader',
-                        options: {
-                            useCache: false,
-                            configFileName: 'tsconfig.demo.json'
-                        }
-                    },
-                    {
-                        loader: 'angular2-template-loader'
-                    }
-                ],
-                exclude: [/\.(spec|e2e)\.ts$/]
-            },
-
-            /**
-             * Raw loader support for *.html
-             * Returns file content as string
-             *
-             * See: https://github.com/webpack/raw-loader
-             */
-            {
-                test: /\.html$/,
-                loader: 'raw-loader',
-                exclude: [path.resolve(__dirname, './src/demo/index.html')]
-            },
-
-            /**
-             * Replace paths
-             *
-             * see: https://github.com/Va1/string-replace-loader
-             */
-            {
-                test: [path.resolve(__dirname, 'src/main/ngx-emoji.module.ts')],
-                loader: 'string-replace-loader',
-                query: {
-                    search: 'ngx-emoji/ngx-emoji.min.css',
-                    replace: 'main/ngx-emoji.less'
-                }
-            },
-            {
-                test: [path.resolve(__dirname, 'src/main/ngx-emoji.service.ts')],
-                loader: 'string-replace-loader',
-                query: {
-                    search: 'ngx-emoji/emojis.json',
-                    replace: '../../build-emoji/emojis.json'
-                }
-            },
-            {
-                test: [path.resolve(__dirname, 'src/demo/app.module.ts')],
-                loader: 'string-replace-loader',
-                query: {
-                    search: 'ngx-emoji-assets',
-                    replace: ''
-                }
-            },
-
-            /**
-             * Less loader
-             *
-             * See: https://github.com/webpack-contrib/less-loader#examples
-             */
-            {
-                test: /\.less$/,
-                use: extractLess.extract({
-                    use: [{
-                        loader: "css-loader"
-                    }, {
-                        loader: "less-loader"
-                    }],
-                    // use style-loader in development
-                    fallback: "style-loader"
-                })
-            }
-        ]
-    },
-
-    /**
-     * Add additional plugins to the compiler.
-     *
-     * See: http://webpack.github.io/docs/configuration.html#plugins
-     */
-    plugins: [
-        /**
-         * Plugin: ContextReplacementPlugin
-         * Description: Provides context to Angular's use of System.import
-         *
-         * See: https://webpack.github.io/docs/list-of-plugins.html#contextreplacementplugin
-         * See: https://github.com/angular/angular/issues/11580
-         */
-        new ContextReplacementPlugin(
-            // The (\\|\/) piece accounts for path separators in *nix and Windows
-            // For Angular 5, see also https://github.com/angular/angular/issues/20357#issuecomment-343683491
-            /\@angular(\\|\/)core(\\|\/)esm5/,
-            path.resolve(__dirname, 'src/demo'), // location of your src
-            {}
-        ),
+        entry: {
+            'demo': path.resolve(__dirname, './src/demo/main.browser.ts')
+        },
 
         /**
-         * Plugin: HtmlWebpackPlugin
-         * Description: Simplifies creation of HTML files to serve your webpack bundles.
-         * This is especially useful for webpack bundles that include a hash in the filename
-         * which changes every compilation.
+         * Options affecting the output of the compilation.
          *
-         * See: https://github.com/ampedandwired/html-webpack-plugin
+         * See: http://webpack.github.io/docs/configuration.html#output
          */
-        new HtmlWebpackPlugin({
-            template: path.resolve(__dirname, './src/demo/index.html'),
-            chunksSortMode: 'dependency',
-            inject: 'body'
-        }),
+        output: {
+            /**
+             * The output directory as absolute path (required).
+             *
+             * See: http://webpack.github.io/docs/configuration.html#output-path
+             */
+            path: path.resolve(__dirname, './dist-demo'),
+
+            /**
+             * Specifies the name of each output file on disk.
+             * IMPORTANT: You must not specify an absolute path here!
+             *
+             * See: http://webpack.github.io/docs/configuration.html#output-filename
+             */
+            filename: '[name].[chunkhash].js',
+
+            /**
+             * Chunk filename with hash.
+             *
+             * See: https://github.com/webpack/webpack/tree/master/examples/chunkhash#webpackconfigjs
+             */
+            chunkFilename: "[chunkhash].js",
+
+            /**
+             * The filename of the SourceMaps for the JavaScript files.
+             * They are inside the output.path directory.
+             *
+             * See: http://webpack.github.io/docs/configuration.html#output-sourcemapfilename
+             */
+            sourceMapFilename: '[file].map',
+
+            /**
+             * Export the bundle as library
+             * See: https://webpack.js.org/configuration/output/#output-librarytarget
+             */
+            libraryTarget: 'var'
+        },
 
         /**
-         * Extract Less plugin
+         * Options affecting the resolving of modules.
          *
-         * See: https://github.com/webpack-contrib/less-loader#in-production
+         * See: http://webpack.github.io/docs/configuration.html#resolve
          */
-        extractLess
-    ],
+        resolve: {
+            /*
+             * An array of extensions that should be used to resolve modules.
+             *
+             * See: http://webpack.github.io/docs/configuration.html#resolve-extensions
+             */
+            extensions: ['.ts', '.js', '.json'],
 
-    /**
-     * Webpack Development Server configuration
-     * Description: The webpack-dev-server is a little node.js Express server.
-     * The server emits information about the compilation state to the client,
-     * which reacts to those events.
-     *
-     * See: https://webpack.github.io/docs/webpack-dev-server.html
-     */
-    devServer: {
-        host: 'localhost',
-        port: 3000,
-        contentBase: [
-            path.resolve(__dirname, 'build-emoji')
-        ],
-        watchOptions: {
-            ignored: [
-                path.resolve(__dirname, '.*')
+            // An array of directory names to be resolved to the current directory
+            modules: [
+                path.resolve(__dirname, './src'),
+                path.resolve(__dirname, './node_modules')
             ]
         },
-        historyApiFallback: true
-    },
 
-    /**
-     * Include polyfills or mocks for various node stuff
-     * Description: Node configuration
-     *
-     * See: https://webpack.github.io/docs/configuration.html#node
-     */
-    node: {
-        global: true,
-        crypto: 'empty',
-        process: true,
-        module: false,
-        clearImmediate: false,
-        setImmediate: false
+        /*
+         * Options affecting the normal modules.
+         *
+         * See: http://webpack.github.io/docs/configuration.html#module
+         */
+        module: {
+            /**
+             * An array of applied pre and post loaders.
+             *
+             * See: https://webpack.js.org/configuration/module/#rule-enforce
+             */
+            rules: [
+                /**
+                 * Static analysis linter for TypeScript advanced options configuration
+                 * Description: An extensible linter for the TypeScript language.
+                 *
+                 * See: https://github.com/wbuchwalter/tslint-loader
+                 */
+                {
+                    test: /\.ts$/,
+                    enforce: 'pre',
+                    loader: 'tslint-loader',
+                    options: {
+                        emitErrors: true,
+                        failOnHint: true,
+                        resourcePath: 'src'
+                    }
+                },
+
+                /**
+                 * Typescript loader support for .ts
+                 *
+                 * Component Template/Style integration using `angular2-template-loader`
+                 * Angular 2 lazy loading (async routes) via `ng-router-loader`
+                 *
+                 * `ng-router-loader` expects vanilla JavaScript code, not TypeScript code. This is why the
+                 * order of the loader matter.
+                 *
+                 * See: https://github.com/s-panferov/awesome-typescript-loader
+                 * See: https://github.com/TheLarkInn/angular2-template-loader
+                 * See: https://github.com/shlomiassaf/ng-router-loader
+                 */
+                {
+                    test: /\.ts$/,
+                    use: [
+                        {
+                            loader: 'awesome-typescript-loader',
+                            options: {
+                                useCache: false,
+                                configFileName: 'tsconfig.demo.json'
+                            }
+                        },
+                        {
+                            loader: 'angular2-template-loader'
+                        }
+                    ],
+                    exclude: [/\.(spec|e2e)\.ts$/]
+                },
+
+                /**
+                 * Raw loader support for *.html
+                 * Returns file content as string
+                 *
+                 * See: https://github.com/webpack/raw-loader
+                 */
+                {
+                    test: /\.html$/,
+                    loader: 'raw-loader',
+                    exclude: [path.resolve(__dirname, './src/demo/index.html')]
+                },
+
+                /**
+                 * Replace paths
+                 *
+                 * see: https://github.com/Va1/string-replace-loader
+                 */
+                {
+                    test: [path.resolve(__dirname, 'src/main/ngx-emoji.module.ts')],
+                    loader: 'string-replace-loader',
+                    query: {
+                        search: 'ngx-emoji/ngx-emoji.min.css',
+                        replace: 'main/ngx-emoji.less'
+                    }
+                },
+                {
+                    test: [path.resolve(__dirname, 'src/main/ngx-emoji.service.ts')],
+                    loader: 'string-replace-loader',
+                    query: {
+                        search: 'ngx-emoji/emojis.json',
+                        replace: '../../build-emoji/emojis.json'
+                    }
+                },
+
+                /**
+                 * Less loader
+                 *
+                 * See: https://github.com/webpack-contrib/less-loader#examples
+                 */
+                {
+                    test: /\.less$/,
+                    use: extractLess.extract({
+                        use: [{
+                            loader: "css-loader"
+                        }, {
+                            loader: "less-loader"
+                        }],
+                        // use style-loader in development
+                        fallback: "style-loader"
+                    })
+                }
+            ]
+        },
+
+        /**
+         * Add additional plugins to the compiler.
+         *
+         * See: http://webpack.github.io/docs/configuration.html#plugins
+         */
+        plugins: [
+            /**
+             * Plugin: ContextReplacementPlugin
+             * Description: Provides context to Angular's use of System.import
+             *
+             * See: https://webpack.github.io/docs/list-of-plugins.html#contextreplacementplugin
+             * See: https://github.com/angular/angular/issues/11580
+             */
+            new ContextReplacementPlugin(
+                // The (\\|\/) piece accounts for path separators in *nix and Windows
+                // For Angular 5, see also https://github.com/angular/angular/issues/20357#issuecomment-343683491
+                /\@angular(\\|\/)core(\\|\/)esm5/,
+                path.resolve(__dirname, 'src/demo'), // location of your src
+                {}
+            ),
+
+            /**
+             * Plugin: HtmlWebpackPlugin
+             * Description: Simplifies creation of HTML files to serve your webpack bundles.
+             * This is especially useful for webpack bundles that include a hash in the filename
+             * which changes every compilation.
+             *
+             * See: https://github.com/ampedandwired/html-webpack-plugin
+             */
+            new HtmlWebpackPlugin({
+                template: path.resolve(__dirname, './src/demo/index.html'),
+                chunksSortMode: 'dependency',
+                inject: 'body'
+            }),
+
+            /**
+             * Extract Less plugin
+             *
+             * See: https://github.com/webpack-contrib/less-loader#in-production
+             */
+            extractLess
+        ],
+
+        /**
+         * Webpack Development Server configuration
+         * Description: The webpack-dev-server is a little node.js Express server.
+         * The server emits information about the compilation state to the client,
+         * which reacts to those events.
+         *
+         * See: https://webpack.github.io/docs/webpack-dev-server.html
+         */
+        devServer: {
+            host: 'localhost',
+            port: 3000,
+            contentBase: [
+                path.resolve(__dirname, 'build-emoji')
+            ],
+            watchOptions: {
+                ignored: [
+                    path.resolve(__dirname, '.*')
+                ]
+            },
+            historyApiFallback: true
+        },
+
+        /**
+         * Include polyfills or mocks for various node stuff
+         * Description: Node configuration
+         *
+         * See: https://webpack.github.io/docs/configuration.html#node
+         */
+        node: {
+            global: true,
+            crypto: 'empty',
+            process: true,
+            module: false,
+            clearImmediate: false,
+            setImmediate: false
+        }
+    };
+
+    if (isDevServer) {
+        config.module.rules.push({
+            test: [path.resolve(__dirname, 'src/demo/app.module.ts')],
+            loader: 'string-replace-loader',
+            query: {
+                search: 'ngx-emoji-assets',
+                replace: ''
+            }
+        });
     }
+
+    return config;
 };
